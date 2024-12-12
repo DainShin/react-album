272c4f2b3c8d160c0d3b05239b01af025ce65971
DainShin <di.shin0611@gmail.com>
2024-12-12 10:46:18 +0900

    added api call in the index.tsx

--- src/components/common/header/CommonHeader.tsx
+++ src/components/common/header/CommonHeader.tsx
@@ -2,7 +2,7 @@ import styles from './CommonHeader.module.scss'
 
 function CommonHeader() {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <div className={styles.header__logoBox}>
         <img src="src/assets/images/image-logo.png" className={styles.header__logoBox__logo} alt="" />
         <span className={styles.header__logoBox__title}>PhotoSplash</span>
@@ -12,7 +12,7 @@ function CommonHeader() {
         <button className={styles.header__profileBox__button}>Book Mark</button>
         <span className={styles.header__profileBox__userName}>Dain | Dain@Dontan</span>
       </div>
-    </div>
+    </header>
   )
 }
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,35 @@
 import styles from "./styles/index.module.scss"
-
 import CommonHeader from "@/components/common/header/CommonHeader"
 import CommonSearchBar from "@/components/common/searchBar/CommonSearchBar"
 import CommonNav from "@/components/common/navigation/CommonNav"
 import CommonFooter from "@/components/common/footer/CommonFooter"
 import Card from "./components/Card"
+import axios from "axios"
+import { useEffect } from "react"
 
 function index() {
+    const getData = async () => {
+        // open api call
+        const API_URL = 'https://api.unsplash.com/search/photos'
+        const API_KEY = 'vxYKGewqhFQZCd3zZ_qRZjSUbDplo_wBykf3bDzf6Ow'
+        const PER_PAGE = 30
+
+        const searchValue = 'Korea'
+        const pageValue = 100
+        
+        try {
+            const res = await axios.get(`${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`)
+            
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
   return (
     <div className={styles.page}>
         {/* 공통 헤더 UI 부분 */}
