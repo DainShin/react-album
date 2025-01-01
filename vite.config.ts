1ef09e6d0fe042ec68d51e8570525de0d62f3f14
DainShin <di.shin0611@gmail.com>
2025-01-01 11:56:06 +0900

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [react()],
   resolve: {
     alias: {
