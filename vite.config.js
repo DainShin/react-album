b767bbea1d3d5b4e9af908734a114b9c050c3653
DainShin <di.shin0611@gmail.com>
2025-01-01 12:02:36 +0900

    created build folder

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+// https://vite.dev/config/
+export default defineConfig({
+    build: {
+        outDir: 'build'
+    },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+            '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+            '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+            '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+            '@recoil': fileURLToPath(new URL('./src/recoil', import.meta.url)),
+            '@apis': fileURLToPath(new URL('./src/apis', import.meta.url)),
+            'react-simple-toasts/theme-dark': '/node_modules/react-simple-toasts/dist/theme/dark.css',
+        },
+    },
+    // SCSS global
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: "@import \"@/assets/styles/main.scss\";",
+            },
+        },
+    },
+});
